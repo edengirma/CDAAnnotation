b5dbeec0e01cf3a95004979b84fccf19f54dcff7
Eden Girma <edengirma@live.com>
2022-01-27 18:22:23 -0500

    Update date argument

--- annotate/__main__.py
+++ annotate/__main__.py
@@ -5,6 +5,6 @@ from .args import args_setup
 
 parser = args_setup()
 pargs = parser.parse_args()
-kwargs = {"archive": pargs.a, "classifier": pargs.c, "days_ago": pargs.d, "novel_objects": bool(pargs.n)}
+kwargs = {"archive": pargs.a, "classifier": pargs.c, "date": pargs.d, "novel_objects": bool(pargs.n)}
 
 program.run(**kwargs)
