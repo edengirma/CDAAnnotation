1ae6fa3950276fa49ab63e94bfa2301ab6b3d7e1
Eden Girma <edengirma@live.com>
2022-01-27 18:08:07 -0500

    Update 'date' argument in program

--- annotate/program.py
+++ annotate/program.py
@@ -1,17 +1,18 @@
 from .functions import ALERCE_DB_query, mocFilter, retrieve_ZTF_CXC_data, append_data, export_data
 from .config import log_path, moc_path, cda_output_path, csc_output_path
 import logging
+from datetime import datetime
 
 # Configure logger
 logging.basicConfig(filename=log_path, encoding='utf-8', format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 
 # Main program run method
-def run(archive='CDA', classifier='lightcurve', days_ago=2.0, novel_objects=True):
+def run(archive='CDA', classifier='lightcurve', date=datetime.today().strftime("%Y-%m-%d"), novel_objects=True):
     try:
         logging.info('######## PROGRAM RUN START ########')
-        logging.info(f'######## ARCHIVE={archive} CLASSIFIER={classifier} DAYS_AGO={days_ago} NOVEL_OBJECTS={novel_objects} ########')
+        logging.info(f'######## ARCHIVE={archive} CLASSIFIER={classifier} DATE={date} NOVEL_OBJECTS={novel_objects} ########')
         logging.info('Querying ALeRCE broker...')
-        apiobjects = ALERCE_DB_query(classifier, days_ago, novel_objects)
+        apiobjects = ALERCE_DB_query(classifier, date, novel_objects)
         logging.info(f'Total ZTF objects retrieved: {len(apiobjects)}')
         if len(apiobjects)==0:
             logging.info('Done; no ZTF objects retrieved from ALeRCE.')
