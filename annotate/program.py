49b2ce624ad9a272dec537255f99c90ddca5befa
Eden Girma <eden.girma@cfa.harvard.edu>
2022-01-27 20:56:58 -0500

    Reformatting logger

--- annotate/program.py
+++ annotate/program.py
@@ -4,42 +4,46 @@ import logging
 from datetime import datetime
 
 # Configure logger
-logging.basicConfig(filename=log_path, encoding='utf-8', format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+FORMAT='%(asctime)s - %(levelname)s - %(message)s'
+DATEFMT='%m/%d/%Y %I:%M:%S %p'
+logging.basicConfig(filename=log_path, encoding='utf-8', format=FORMAT, datefmt=DATEFMT, level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 # Main program run method
 def run(archive='CDA', classifier='lightcurve', date=datetime.today().strftime("%Y-%m-%d"), novel_objects=True):
     try:
-        logging.info('######## PROGRAM RUN START ########')
-        logging.info(f'######## ARCHIVE={archive} CLASSIFIER={classifier} DATE={date} NOVEL_OBJECTS={novel_objects} ########')
-        logging.info('Querying ALeRCE broker...')
+        logger.info('######## PROGRAM RUN START ########')
+        logger.info(f'######## ARCHIVE={archive} CLASSIFIER={classifier} DATE={date} NOVEL_OBJECTS={novel_objects} ########')
+        logger.info('Querying ALeRCE broker...')
         apiobjects = ALERCE_DB_query(classifier, date, novel_objects)
-        logging.info(f'Total ZTF objects retrieved: {len(apiobjects)}')
+        logger.info(f'Total ZTF objects retrieved: {len(apiobjects)}')
         if len(apiobjects)==0:
-            logging.info('Done; no ZTF objects retrieved from ALeRCE.')
-            logging.info('######## PROGRAM RUN END ########')
+            logger.info('Done; no ZTF objects retrieved from ALeRCE.')
+            logger.info('######## PROGRAM RUN END ########')
             return
 
-        logging.info('Filtering table with Chandra MOC...')
+        logger.info('Filtering table with Chandra MOC...')
         ztfobjects = mocFilter(apiobjects, moc_path)
-        logging.info(f'Total ZTF objects passed through filter: {len(ztfobjects)}')
+        logger.info(f'Total ZTF objects passed through filter: {len(ztfobjects)}')
         if len(ztfobjects)==0:
-            logging.info('Done; no ZTF object positions lie in Chandra MOC.')
-            logging.info('######## PROGRAM RUN END ########')
+            logger.info('Done; no ZTF object positions lie in Chandra MOC.')
+            logger.info('######## PROGRAM RUN END ########')
             return
 
-        logging.info('Retrieving CXC data for ZTF objects...')
+        logger.info('Retrieving CXC data for ZTF objects...')
         ztf_fullxmatch = retrieve_ZTF_CXC_data(archive, ztfobjects)
-        logging.info(f'Total Chandra xmatches retrieved:{len(ztf_fullxmatch)}')
+        logger.info(f'Total Chandra xmatches retrieved:{len(ztf_fullxmatch)}')
 
-        logging.info('Outputting data...')
+        logger.info('Outputting data...')
         output_path = {'CDA': cda_output_path,
                        'CSC': csc_output_path}[archive]
         append_data(ztf_fullxmatch, output_path)
 
         # print('Exporting data table')
         # export_data(ztf_fullxmatch, output_path)
-        logging.info('Done; ZTF data outputted.')
-        logging.info('######## PROGRAM RUN END ########')
+        logger.info('Done; ZTF data outputted.')
+        logger.info('######## PROGRAM RUN END ########')
 
     except Exception as err:
-        logging.error(f"Unexpected {err=}, {type(err)=}")
+        logger.exception(err)
+        logger.info('######## PROGRAM RUN END ########')
